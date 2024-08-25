95f02612bcc5200d97e13a74811fc3226f398911
jarossi1 <jarossi91@yahoo.com>
2024-08-25 13:14:43 -0400

    updated spinner added values and BeerExpert class 8/25/24

--- app/src/main/java/com/hfad/mytestapp/MainActivity.java
+++ app/src/main/java/com/hfad/mytestapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.hfad.mytestapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +11,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
+    private BeerExpert expert = new BeerExpert();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +26,26 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+    }
+    public void onClickFindBeer(View view) {
+        // Get a reference to the TextView
+        TextView brands = (TextView) findViewById(R.id.brands);
+
+        // Get a reference to the Spinner
+        Spinner color = (Spinner) findViewById(R.id.color);
+
+        // Get the selected item in the Spinner, Convert to a string
+        String beerType = String.valueOf(color.getSelectedItem());
+
+        // Get recommendations from the BeerExpert class
+        List<String> brandsList = expert.getBrands(beerType);
+        StringBuilder brandsFormatted = new StringBuilder();
+        for (String brand : brandsList) {
+            brandsFormatted.append(brand).append('\n');
+        }
+        //display the beers
+        brands.setText(brandsFormatted);
+
     }
 }
